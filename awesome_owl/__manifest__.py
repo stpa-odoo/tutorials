fcbf4ef999e07682dd54657c97814c3f7253ecb2
fdardenne <dafl@odoo.com>
2023-11-06 10:06:34 +0100

    v16

--- awesome_owl/__manifest__.py
+++ awesome_owl/__manifest__.py
@@ -32,22 +32,22 @@
             ('include', 'web._assets_helpers'),
             'web/static/src/scss/pre_variables.scss',
             'web/static/lib/bootstrap/scss/_variables.scss',
-            ('include', 'web._assets_bootstrap_backend'),
+            ('include', 'web._assets_bootstrap'),
 
-            # required for fa icons
-            'web/static/src/libs/fontawesome/css/font-awesome.css',
-            
-            # include base files from framework
-            ('include', 'web._assets_core'),
-
-            # remove some files that we do not use to create a minimal bundle
-            ('remove', 'web/static/src/core/**/*'),
-            ('remove', 'web/static/lib/luxon/luxon.js'),
+            'web/static/src/libs/fontawesome/css/font-awesome.css', # required for fa icons
+            'web/static/src/legacy/js/promise_extension.js', # required by boot.js
+            'web/static/src/boot.js', # odoo module system
+            'web/static/src/env.js', # required for services
+            'web/static/src/session.js', # expose __session_info__ containing server information
+            'web/static/lib/owl/owl.js', # owl library
+            'web/static/lib/owl/odoo_module.js', # to be able to import "@odoo/owl"
             'web/static/src/core/utils/functions.js',
             'web/static/src/core/browser/browser.js',
             'web/static/src/core/registry.js',
             'web/static/src/core/assets.js',
             'awesome_owl/static/src/**/*',
+
+            
         ],
     },
     'license': 'AGPL-3'
